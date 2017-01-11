a66570427d82e191dbc94dc306e610877a48feb4
jennifernghinguyen <jennifernghi@yahoo.com>
2017-01-11 14:57:02 -0800

    book_list_item_view.xml

--- app/src/main/java/com/example/android/booklisting/BookActivity.java
+++ app/src/main/java/com/example/android/booklisting/BookActivity.java
@@ -3,11 +3,11 @@ package com.example.android.booklisting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BookActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.book_list_view);
     }
 }
